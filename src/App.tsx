bee28750b30b956394818051017302f5656b849d
Volnei Tonato <volneitonato@gmail.com>
2020-06-08 22:43:25 -0300

    Ajuste para o App.ts chamar a rota e nao mais o home direto

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { store } from './store'
-import HomePage from './pages/Home'
 import Layout from './components/Layout'
+import Routes from './routes'
+
+
 const App = () => {
   return (
     <Provider store={store}>
       <Layout>
-        <HomePage />
+        <Routes />
       </Layout>
     </Provider>
   )
